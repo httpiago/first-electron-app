13692da223fa2f21f8a49b02f8a24c9661a7f88c
Iago Bruno <httpiago@gmail.com>
2018-01-29 15:26:19 -0300

    Remoção de uma constante repetida

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-const app = require('app');
-
 // this should be placed at top of main.js to handle setup events quickly
 if ( handleSquirrelEvent() ) {
   // squirrel event handled and app will exit in 1000ms, so don't do anything else
