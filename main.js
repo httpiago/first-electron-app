0b1eca4e90e922ebad07907650bd92618d08c699
Iago Bruno <httpiago@gmail.com>
2018-01-29 04:02:52 -0300

    Separar a janela de instalação da janela principal
    Ao iniciar pela primeira vez, o programa irá exibir uma janela constumizada de instalação, da próxima vez, ele irá abrir o programa em sí

--- main.js
+++ main.js
@@ -11,10 +11,7 @@ const url = require('url');
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow;
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', function createWindow() {
+function createMainWindow() {
 	// Create the browser window.
 	mainWindow = new BrowserWindow({width: 800, height: 600})
 
@@ -36,6 +33,56 @@ app.on('ready', function createWindow() {
 		// when you should delete the corresponding element.
 		mainWindow = null
 	});
+}
+
+function installWindow () {
+	
+	// Criar a janela de instalção
+	let installWindow = new BrowserWindow({
+		transparent: false,
+		frame: true, // Mostrar (ou não) somente o conteúdo html da janela
+		titleBarStyle: 'hiddenInset',
+		width: 800,
+		height: 600,
+		resizable: false,
+		maximizable: false,
+		closable: true,
+		alwaysOnTop: false,
+		title: app.getName(),
+	});
+	
+	let window_url = url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	});
+	installWindow.loadURL( window_url );
+	
+	installWindow.on('closed', () => {
+		
+		// Cancelar a instalaçãp
+		installWindow = null;
+		
+	});
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', function () {
+	
+	
+	if ( true ) {
+		
+		// Iniciar a instalação
+		installWindow();
+		
+	} else {
+		
+		// Já instalado iniciar o aplicativo
+		createMainWindow();
+		
+	}
 });
 
 // Quit when all windows are closed.
